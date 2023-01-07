7876d764668f766a8b0c86b05cecc32312d597d2
Salam4nder <kamran.personal@icloud.com>
2023-01-07 03:41:55 +0100

    add function to create new instance of DB

--- pkg/persistence/persistence.go
+++ pkg/persistence/persistence.go
@@ -1,3 +1,33 @@
 package persistence
 
-type Storage interface {}
+import (
+	"database/sql"
+
+	"github.com/Salam4nder/inventory/config"
+)
+
+// Storage is a persistence layer interface.
+type Storage interface{}
+
+// Database is a persistence layer implementing
+// the Storage interface.
+type Database struct {
+	DB     *sql.DB
+	Config config.Database
+}
+
+// New creates a new Database instance from the given
+// config.Database instance.
+func New(dbCfg *config.Database) (*Database, error) {
+	db, err := sql.Open("postgres", dbCfg.Connection())
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return &Database{DB: db, Config: *dbCfg}, nil
+	//TODO: defer db.Close() in entry point
+}
