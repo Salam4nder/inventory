55cfe015eaea278d96c62f3b26e08852f0d5b7e4
Salam4nder <kamran.personal@icloud.com>
2023-01-11 00:39:23 +0100

    rename api -> http

---
+++ pkg/http/server.go
@@ -0,0 +1,35 @@
+package http
+
+import (
+	"github.com/Salam4nder/inventory/config"
+	"github.com/Salam4nder/inventory/pkg/inventory"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Server is the main structure of the API.
+type Server struct {
+	Config  config.Server
+	Service inventory.Service
+}
+
+// New creates a new instance of the API server.
+func New(cfg config.Server, srvc inventory.Service) *Server {
+	return &Server{
+		Config:  cfg,
+		Service: srvc,
+	}
+}
+
+// Start starts the server.
+func (s *Server) Start() error {
+	router := gin.Default()
+	router.GET("/health", s.health)
+	router.GET("/item/:id", s.getItem)
+	router.GET("/items", s.getItems)
+	router.POST("/item", s.createItem)
+	router.PUT("/item/:id", s.updateItem)
+	router.DELETE("/item/:id", s.deleteItem)
+
+	return router.Run(":" + s.Config.Port)
+}
