c26842939dbe842ce086515cfebb33507d4af859
Salam4nder <kamran.personal@icloud.com>
2023-01-11 01:00:29 +0100

    update Server struct fields to unexported, rename endpoints in Start

--- internal/http/server.go
+++ internal/http/server.go
@@ -2,22 +2,22 @@ package http
 
 import (
 	"github.com/Salam4nder/inventory/config"
-	"github.com/Salam4nder/inventory/pkg/inventory"
+	"github.com/Salam4nder/inventory/internal/inventory"
 
 	"github.com/gin-gonic/gin"
 )
 
 // Server is the main structure of the API.
 type Server struct {
-	Config  config.Server
-	Service inventory.Service
+	config  config.Server
+	service inventory.Service
 }
 
 // New creates a new instance of the API server.
 func New(cfg config.Server, srvc inventory.Service) *Server {
 	return &Server{
-		Config:  cfg,
-		Service: srvc,
+		config:  cfg,
+		service: srvc,
 	}
 }
 
@@ -25,11 +25,11 @@ func New(cfg config.Server, srvc inventory.Service) *Server {
 func (s *Server) Start() error {
 	router := gin.Default()
 	// router.GET("/health", s.health)
-	router.GET("/item/:id", s.getItem)
-	router.GET("/item", s.getItems)
+	router.GET("/item/:id", s.readItem)
+	router.GET("/item", s.readItems)
 	router.POST("/item", s.createItem)
 	router.PUT("/item/:id", s.updateItem)
 	router.DELETE("/item/:id", s.deleteItem)
 
-	return router.Run(":" + s.Config.Port)
+	return router.Run(":" + s.config.Port)
 }
