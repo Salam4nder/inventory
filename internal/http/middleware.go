dc9ac0f7fc501163717b69bc12c79f2bd2c31d1a
Salam4nder <kamran.personal@icloud.com>
2023-01-11 23:49:26 +0100

    add jwt auth middleware

---
+++ internal/http/middleware.go
@@ -0,0 +1,28 @@
+package http
+
+import (
+	"github.com/Salam4nder/inventory/pkg/auth"
+
+	"github.com/gin-gonic/gin"
+)
+
+// JWTAuth is a middleware that checks if the
+// request has a valid JWT token.
+func JWTAuth(secret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.GetHeader("Authorization")
+		if token == "" {
+			c.JSON(401, gin.H{"error": " JWT token required"})
+			c.Abort()
+			return
+		}
+
+		if err := auth.ValidateJWT(secret, token); err != nil {
+			c.JSON(401, gin.H{"error": "JWT token is invalid"})
+			c.Abort()
+			return
+		}
+
+		c.Next()
+	}
+}
