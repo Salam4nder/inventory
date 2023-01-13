4dd066ea9f60af1c4813d340fa2e7cc0c6b171ae
Salam4nder <kamran.personal@icloud.com>
2023-01-13 07:56:07 +0100

    add New function in logger.go

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,4 +1,13 @@
 package logger
 
-// Logger is the main interface for logging
-type Logger interface{}
+import "go.uber.org/zap"
+
+// New returns a new logger
+func New() (*zap.Logger, error) {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return nil, err
+	}
+
+	return logger, nil
+}
