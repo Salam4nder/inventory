f0ec887a3712b6613ffff69cc99c63ac74b0e442
Salam4nder <kamran.personal@icloud.com>
2023-01-14 03:38:00 +0100

    updated rest crud handlers

--- internal/http/handler.go
+++ internal/http/handler.go
@@ -5,11 +5,16 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/Salam4nder/inventory/internal/service/entity"
 	"github.com/gin-gonic/gin"
 )
 
 func (s *Server) readItem(c *gin.Context) {
-	uuid := c.Param("id")
+	uuid, found := c.Params.Get("uuid")
+	if !found {
+		c.JSON(http.StatusBadRequest, "uuid not found")
+		return
+	}
 
 	ctx, cancel := context.WithTimeout(
 		context.Background(), 5*time.Second)
@@ -29,13 +34,65 @@ func (s *Server) readItems(c *gin.Context) {
 }
 
 func (s *Server) createItem(c *gin.Context) {
-	//TODO: implement
+	var item entity.Item
+
+	if err := c.ShouldBindJSON(&item); err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(), 5*time.Second)
+	defer cancel()
+
+	uuid, err := s.service.Create(ctx, item)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, uuid)
+
 }
 
 func (s *Server) updateItem(c *gin.Context) {
-	//TODO: implement
+	item := &entity.Item{}
+
+	if err := c.ShouldBindJSON(item); err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(), 5*time.Second)
+	defer cancel()
+
+	updatedItem, err := s.service.Update(ctx, item)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, updatedItem)
 }
 
 func (s *Server) deleteItem(c *gin.Context) {
-	//TODO: implement
+	uuid, found := c.Params.Get("uuid")
+	if !found {
+		c.JSON(http.StatusBadRequest, "uuid not found")
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := s.service.Delete(ctx, uuid)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.JSON(http.StatusOK,
+		gin.H{"deleted": uuid})
 }
