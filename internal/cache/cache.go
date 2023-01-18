76cc45dd93040fd68b3477de2fdf81fde3916316
Salam4nder <kamran.personal@icloud.com>
2023-01-18 11:52:58 +0100

    add redis cache

---
+++ internal/cache/cache.go
@@ -0,0 +1,49 @@
+package cache
+
+import (
+	"context"
+	"time"
+
+	"github.com/Salam4nder/inventory/config"
+
+	"github.com/go-redis/redis/v8"
+)
+
+// Cache is an abstract interface for a cache.
+type Cache interface {
+	Delete(key string) int64
+	Set(
+		key string, value interface{}, expiryTime time.Duration) error
+	Get(Key string) string
+}
+
+// Redis is an implementation of Cache interface.
+type Redis struct {
+	Client *redis.Client
+}
+
+// New returns a new instance of Redis.
+func New(cfg config.Cache) *Redis {
+	return &Redis{
+		Client: redis.NewClient(&redis.Options{
+			Addr: cfg.Host,
+		}),
+	}
+}
+
+// Get returns the value of the key.
+func (c *Redis) Get(ctx context.Context, Key string) string {
+	return c.Client.Get(ctx, Key).Val()
+}
+
+// Set sets the value of the key.
+func (c *Redis) Set(
+	ctx context.Context, key string, value interface{},
+	expiryTime time.Duration) error {
+	return c.Client.Set(ctx, key, value, expiryTime).Err()
+}
+
+// Delete deletes the key.
+func (c *Redis) Delete(ctx context.Context, key string) int64 {
+	return c.Client.Del(ctx, key).Val()
+}
