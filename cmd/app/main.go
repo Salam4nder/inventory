0224a16e2d9e84db2df801ff7f1199f73e0b8c78
Salam4nder <kamran.personal@icloud.com>
2023-02-05 17:35:03 +0100

    update main

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/Salam4nder/inventory/config"
+	"github.com/Salam4nder/inventory/internal/domain"
+	"github.com/Salam4nder/inventory/internal/http"
+	"github.com/Salam4nder/inventory/internal/persistence"
+	"github.com/Salam4nder/inventory/pkg/logger"
+
+	"github.com/stimtech/go-migration"
+)
 
 func main() {
-    fmt.Println("wip")
+	cfg, err := config.New()
+	panicOnError(err)
+
+	logger, err := logger.New("")
+	panicOnError(err)
+
+	PSQL, err := persistence.New(
+		&cfg.DB, persistence.PostgresDriver)
+	panicOnError(err)
+	defer PSQL.DB.Close()
+	logger.Info("Database connection established...")
+
+	migration := migration.New(
+		PSQL.DB, logger)
+	if err := migration.Migrate(); err != nil {
+		panicOnError(err)
+	}
+
+	service := domain.New(PSQL)
+
+	server := http.New(cfg.HTTP, service, logger)
+	server.Start()
+}
+
+func panicOnError(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
