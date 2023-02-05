9780214211c192c1eb5e86df8e7a6dd2b5a1f10c
Salam4nder <kamran.personal@icloud.com>
2023-02-05 17:23:00 +0100

    add initial migrations file

--- db/migrations/2023-01-25-initial-db.sql
+++ db/migrations/2023-01-25-initial-db.sql
@@ -1,7 +1,9 @@
-CREATE TABLE IF NOT EXISTS item (
-    id uuid PRIMARY KEY,
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS inventory (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     name varchar(255) NOT NULL,
     unit varchar(255) NOT NULL,
     amount double precision NOT NULL,
-    expires_at timestamptz NOT NULL,
+    expires_at timestamptz NOT NULL
 );
